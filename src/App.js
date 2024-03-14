4f22f2b9fa8696d8e3979187126102607416e76d
Manuel Lera Ramirez <manulera14@gmail.com>
2024-03-14 15:28:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useScript } from "usehooks-ts";
 
 function App() {
+  const status = useScript(`https://apis.google.com/js/api.js`, {
+    removeOnUnmount: false,
+    id: "gapi",
+  });
+
+  const status2 = useScript(`https://accounts.google.com/gsi/client`, {
+    removeOnUnmount: false,
+    id: "google",
+  });
+
+  React.useEffect(() => {
+    if (typeof gapi !== "undefined") {
+      console.log("gapi is", gapi.load);
+    }
+    if (typeof google !== "undefined") {
+      console.log("google is", google.accounts);
+    }
+  }, [status, status2]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello</div>
     </div>
   );
 }
