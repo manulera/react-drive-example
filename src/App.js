baa3d6dfb438461cea01ea14d26f236f7533b44e
AryanKenia <aryan.kenia.20@ucl.ac.uk>
2024-03-19 16:17:49 +0000

    Code reorganised and sterwamlined.  Now using goog lePickerFunctions.js to handle all the google pick er functions.

--- src/App.js
+++ src/App.js
@@ -1,196 +1,42 @@
 import React from "react";
 import "./App.css";
-import { useScript } from "usehooks-ts";
 import GoogleDriveComponent from "./GoogleDriveComponent";
+import {
+  readFileFromGooglePicker,
+  submitFileToGoogleDrive,
+} from "./googlePickerFunctions";
 
-let accessToken = null;
 function App() {
   const [scriptVars, setScriptVars] = React.useState({ loaded: false });
   const [inTextArea, setInTextArea] = React.useState("");
   const [userFileName, setUserFileName] = React.useState("");
 
-
-
-async function pickerCallback(data) {
-  console.log(data);
-  let url = "nothing";
-  const resp = await fetch("/keys.json");
-  const { apiKey } = await resp.json();
-  if (
-    data[google.picker.Response.ACTION] == google.picker.Action.PICKED
-  ) {
-    let doc = data[google.picker.Response.DOCUMENTS][0];
-    console.log(data.docs[0].id);
-    url = doc[google.picker.Document.URL];
-    
-    fetch(
-      `https://www.googleapis.com/drive/v3/files/${data.docs[0].id}?key={apiKey}&alt=media`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + accessToken,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        return JSON.stringify(data)})
-      .then((data) => {
-        console.log(data);
-        setInTextArea(data);
-      });
-  }
-}
-
-async function createPicker(scriptVars) {
-  console.log(scriptVars);
-  const { loaded, apiKey, tokenClient} = scriptVars;
-  if (loaded === true) {
-        const showPicker = async () => {
-          // TODO(developer): Replace with your API key
-          const picker = new google.picker.PickerBuilder()
-            .addView(google.picker.ViewId.DOCS)
-            .setOAuthToken(accessToken)
-            .setDeveloperKey(apiKey)
-            .setCallback(pickerCallback)
-            .build();
-          picker.setVisible(true);
-        }; 
-
-        // Request an access token.
-        tokenClient.callback = async (response) => {
-          if (response.error !== undefined) {
-            throw response;
-          }
-          accessToken = response.access_token;
-          showPicker();
-        };
-
-        if (accessToken === null) {
-          // Prompt the user to select a Google Account and ask for consent to share their data
-          // when establishing a new session.
-          tokenClient.requestAccessToken({ prompt: "consent" });
-        } else {
-          // Skip display of account chooser and consent dialog for an existing session.
-          tokenClient.requestAccessToken({ prompt: "" });
-        }
-      }
-    }
-
-async function createSubmitPicker(scriptVars) {
-  console.log(scriptVars);
-  const { loaded, apiKey, tokenClient } = scriptVars;
- 
-  if (loaded === true) {
-    const showPicker = () => {
-      // TODO(developer): Replace with your API key
-      const view = new google.picker.DocsView(
-        google.picker.ViewId.FOLDERS
-      ).setSelectFolderEnabled(true);
-      const picker = new google.picker.PickerBuilder()
-        .addView(view)
-        .setOAuthToken(accessToken)
-        .setDeveloperKey(apiKey)
-        .setCallback(pickerSubmitCallback)
-        .build();
-      picker.setVisible(true);
-    };
-
-    // Request an access token.
-    tokenClient.callback = async (response) => {
-      if (response.error !== undefined) {
-        throw response;
-      }
-      accessToken = response.access_token;
-      showPicker();
-    };
-
-    if (accessToken === null) {
-      // Prompt the user to select a Google Account and ask for consent to share their data
-      // when establishing a new session.
-      tokenClient.requestAccessToken({ prompt: "consent" });
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      tokenClient.requestAccessToken({ prompt: "" });
-    }
-  }
-}
-
-async function pickerSubmitCallback(data) {
-  console.log(data);
-        let url = "nothing";
-        const resp = await fetch("/keys.json");
-        const { apiKey } = await resp.json();
-        if (
-          data[google.picker.Response.ACTION] == google.picker.Action.PICKED
-        ) {
-          let doc = data[google.picker.Response.DOCUMENTS][0];
-          let folderID = data.docs[0].id;
-          url = doc[google.picker.Document.URL];
-          
-          let fileName = userFileName;
-          let fileNameJSON = fileName + ".json";
-          let fileContent = inTextArea;
-          const file = new Blob([fileContent], { type: "text/plain" });
-        const metadata = {
-          name: fileNameJSON,
-          mimeType: "text/plain",
-          parents: [folderID], // Google Drive folder id
-        };
-        const form = new FormData();
-        form.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
-        form.append("file", file);
-
-        const updateUrl = `https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&supportsAllDrives=true&key=${apiKey}&uploadType=media`;
-        fetch(updateUrl, {
-          method: "POST",
-          headers: {
-            Authorization: "Bearer " + accessToken,
-          },
-          body: form,
-        })
-          .then((res) => {
-            return res.json();
-          })
-          .then(function (val) {
-            console.log(val);
-          });
-        }
-}
-
-
-
-
-
-
-
-
   const onClickButton = () => {
-    createPicker(scriptVars);
+    readFileFromGooglePicker(scriptVars, setInTextArea);
   };
 
-  const onClickSubmitButton = ()  => {
-    createSubmitPicker(scriptVars);
-  }
-
-
-
+  const onClickSubmitButton = () => {
+    submitFileToGoogleDrive(scriptVars, userFileName, inTextArea);
+  };
 
   return (
     <div className="App">
       <GoogleDriveComponent setScriptVars={setScriptVars} />
 
-
-
       <div>Hello</div>
       <button onClick={onClickButton}>Read File</button>
 
-      <textarea value={inTextArea} onChange={(e) => setInTextArea(e.target.value)}></textarea>
-      <input value={userFileName} onChange={(e) => setUserFileName(e.target.value)}></input>
+      <textarea
+        value={inTextArea}
+        onChange={(e) => setInTextArea(e.target.value)}
+      ></textarea>
+
+      <input
+        value={userFileName}
+        onChange={(e) => setUserFileName(e.target.value)}
+      ></input>
 
       <button onClick={onClickSubmitButton}>Submit File</button>
-      
 
       <div>{scriptVars.loaded ? "loaded" : "not loaded"}</div>
     </div>
